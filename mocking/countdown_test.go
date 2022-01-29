97032bf2eac7241f514ab3aa2278de76a6375233
dontcampy <dontcampy@gmail.com>
2022-01-29 22:21:03 +0800

    Create an observer to record the order in which pauses and prints are issued

--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
 
@@ -12,6 +13,22 @@ type ObservableSleeper struct {
 func (s *ObservableSleeper) Sleep() {
 	s.Calls++
 }
+
+type ObservableCountdownOperations struct {
+	Calls []string
+}
+
+func (s *ObservableCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *ObservableCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const write = "write"
+const sleep = "sleep"
 
 func TestCountdown(t *testing.T) {
 	buffer := &bytes.Buffer{}
@@ -28,4 +45,24 @@ Go!`
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
+
+	t.Run("sleep before every print", func(t *testing.T) {
+		observableSleeperPrinter := &ObservableCountdownOperations{}
+		Countdown(observableSleeperPrinter, observableSleeperPrinter)
+
+		want := []string{
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		if !reflect.DeepEqual(want, observableSleeperPrinter.Calls) {
+			t.Errorf("wanted calls %v got %v", want, observableSleeperPrinter.Calls)
+		}
+	})
 }
