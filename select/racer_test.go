91229f86f15b6a6bb1c3dd36fca3137da47b2b4b
dontcampy <dontcampy@gmail.com>
2022-01-31 23:23:28 +0800

    Use select/case and goroutines to improve the performance and readability of our code

---
+++ select/racer_test.go
@@ -0,0 +1,35 @@
+package race
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestRacer(t *testing.T) {
+	slowServer := makeDelayedServer(20 * time.Microsecond)
+	fastServer := makeDelayedServer(0 * time.Millisecond)
+
+	defer slowServer.Close()
+	defer fastServer.Close()
+
+	slowUrl := slowServer.URL
+	fastUrl := fastServer.URL
+
+	want := fastUrl
+	got := Racer(slowUrl, fastUrl)
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(
+		http.HandlerFunc(
+			func(writer http.ResponseWriter, request *http.Request) {
+				time.Sleep(delay)
+				writer.WriteHeader(http.StatusOK)
+			}))
+}
