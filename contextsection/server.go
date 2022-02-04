53c5e34ba7aa8f81fbfefffb9ab347f94f98fa14
dontcampy <dontcampy@gmail.com>
2022-02-04 16:29:29 +0800

    Cancel by context

--- contextsection/server.go
+++ contextsection/server.go
@@ -1,30 +1,22 @@
 package contextsection
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 )
 
 type Store interface {
-	Fetch() string
-	Cancel()
+	Fetch(ctx context.Context) (string, error)
 }
 
 func Server(store Store) http.HandlerFunc {
 	return func(writer http.ResponseWriter, request *http.Request) {
-		ctx := request.Context()
+		data, err := store.Fetch(request.Context())
 
-		data := make(chan string, 1)
-
-		go func() {
-			data <- store.Fetch()
-		}()
-
-		select {
-		case d := <-data:
-			fmt.Fprint(writer, d)
-		case <-ctx.Done():
-			store.Cancel()
+		if err != nil {
+			return
 		}
+		fmt.Fprint(writer, data)
 	}
 }
